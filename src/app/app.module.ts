ec320ccb08a96406341cda1484fe31f763063f28
Pedro Martini <pederiquemartine@gmail.com>
2022-10-06 17:34:28 -0300

    Tirando códigos desnecessarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { LayoutModule } from './layout/layout.module';
+import { PagesModule } from './pages/pages.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PagesModule,
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
